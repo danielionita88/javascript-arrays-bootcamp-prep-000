a0ae1fce503df8b216997a4c248d27152d553f7d
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-22 22:01:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,5 +22,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array
 }
 function accessElementInArray(array, index) {
-  return array[element];
+  return array[index];
 }
