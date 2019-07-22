13c61671b2d2bd173ab6407faa6d0d312279b332
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-22 22:14:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,3 +40,9 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array;
   
 }
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0,array.length -1);
+  return array.slice(0, array.length -1);
+  
+}
